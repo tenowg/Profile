717f32eaba752f902b7d657ea72e04835d0e3cd4
Craig Russell <tenowg@gmail.com>
2013-02-01 01:51:56 -0500

    Initial Commit

---
+++ src/main/java/com/thedemgel/profile/Profile.java
@@ -0,0 +1,19 @@
+
+package com.thedemgel.profile;
+
+import org.spout.api.plugin.CommonPlugin;
+
+
+public class Profile extends CommonPlugin {
+
+	@Override
+	public void onEnable() {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	@Override
+	public void onDisable() {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+}
